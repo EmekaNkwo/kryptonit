0c5bd19014128c82bb770e99b86ce4a6dd8d723c
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-07-28 23:12:53 +0100

    added the news section to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -61,7 +61,7 @@ function Homepage() {
       <Cryptocurrencies simplified />
       <div className="home-heading-container">
         <Title level={2} className="home-title">
-          Lastest Crypto News
+          Latest Crypto News
         </Title>
         <Title level={3} className="show-more">
           <Link to="/news">Show More</Link>
